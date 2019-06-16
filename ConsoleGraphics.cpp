4420d01099fd7e9ba4006de768fe8588d1466d50
Levi Aviv <leviaviv28@gmail.com>
2019-06-16 12:21:14 -0400

    Added additional drawing features and refactored code

--- ConsoleGraphics.cpp
+++ ConsoleGraphics.cpp
@@ -8,7 +8,7 @@ void ConsoleGraphics::pushInt(int convInt) {
     graphicsBuffer[graphicsBufferSize++] = highByte(convInt);
 }
 
-void ConsoleGraphics::sendGraphics(){
+void ConsoleGraphics::flushBuffer(){
     Wire.beginTransmission(8);
     Wire.write(graphicsBufferSize);
     for(int i = 0; i < graphicsBufferSize; i++){
@@ -19,75 +19,63 @@ void ConsoleGraphics::sendGraphics(){
     graphicsBufferSize = 0;
 }
     
-void ConsoleGraphics::addFastVLine(int x, int y, int length, RGBColor color){
+void ConsoleGraphics::drawFastVLine(int x, int y, int length, byte color){
     graphicsBuffer[graphicsBufferSize++] = 0;
-    graphicsBuffer[graphicsBufferSize++] = color.r;
-    graphicsBuffer[graphicsBufferSize++] = color.g;
-    graphicsBuffer[graphicsBufferSize++] = color.b;
     pushInt(x);
     pushInt(y);
     pushInt(length);
-    sendGraphics();
+    graphicsBuffer[graphicsBufferSize++] = color;
+    flushBuffer();
 }
   
-void ConsoleGraphics::addFastHLine(int x, int y, int length, RGBColor color){
+void ConsoleGraphics::drawFastHLine(int x, int y, int length, byte color){
     graphicsBuffer[graphicsBufferSize++] = 1;
-    graphicsBuffer[graphicsBufferSize++] = color.r;
-    graphicsBuffer[graphicsBufferSize++] = color.g;
-    graphicsBuffer[graphicsBufferSize++] = color.b;
     pushInt(x);
     pushInt(y);
     pushInt(length);
-    sendGraphics();
+    graphicsBuffer[graphicsBufferSize++] = color;
+    flushBuffer();
 }
 
-void ConsoleGraphics::addRect(int x, int y, int w, int h, RGBColor color){
+void ConsoleGraphics::addRect(int x, int y, int w, int h, byte color){
     graphicsBuffer[graphicsBufferSize++] = 2;
-    graphicsBuffer[graphicsBufferSize++] = color.r;
-    graphicsBuffer[graphicsBufferSize++] = color.g;
-    graphicsBuffer[graphicsBufferSize++] = color.b;
     pushInt(x);
     pushInt(y);
     pushInt(w);
     pushInt(h);
-    sendGraphics();
+    graphicsBuffer[graphicsBufferSize++] = color;
+    flushBuffer();
 }
 
-void ConsoleGraphics::fillRect(int x, int y, int w, int h, RGBColor color){
+void ConsoleGraphics::fillRect(int x, int y, int w, int h, byte color){
     graphicsBuffer[graphicsBufferSize++] = 3;
-    graphicsBuffer[graphicsBufferSize++] = color.r;
-    graphicsBuffer[graphicsBufferSize++] = color.g;
-    graphicsBuffer[graphicsBufferSize++] = color.b;
     pushInt(x);
     pushInt(y);
     pushInt(w);
     pushInt(h);
-    sendGraphics();
+    graphicsBuffer[graphicsBufferSize++] = color;
+    flushBuffer();
 }
 
-void ConsoleGraphics::addCircle(int x, int y, int r, RGBColor color){
+void ConsoleGraphics::addCircle(int x, int y, int r, byte color){
     graphicsBuffer[graphicsBufferSize++] = 4;
-    graphicsBuffer[graphicsBufferSize++] = color.b;
-    graphicsBuffer[graphicsBufferSize++] = color.g;
-    graphicsBuffer[graphicsBufferSize++] = color.r;
     pushInt(x);
     pushInt(y);
     pushInt(r);
-    sendGraphics();
+    graphicsBuffer[graphicsBufferSize++] = color;
+    flushBuffer();
 }
 
-void ConsoleGraphics::fillCircle(int x, int y, int r, RGBColor color){
+void ConsoleGraphics::fillCircle(int x, int y, int r, byte color){
     graphicsBuffer[graphicsBufferSize++] = 5;
-    graphicsBuffer[graphicsBufferSize++] = color.b;
-    graphicsBuffer[graphicsBufferSize++] = color.g;
-    graphicsBuffer[graphicsBufferSize++] = color.r;
     pushInt(x);
     pushInt(y);
     pushInt(r);
-    sendGraphics();
+    graphicsBuffer[graphicsBufferSize++] = color;
+    flushBuffer();
 }
 
-void ConsoleGraphics::addTriangle(int x0, int y0, int x1, int y1, int x2, int y2, RGBColor color){
+void ConsoleGraphics::addTriangle(int x0, int y0, int x1, int y1, int x2, int y2, byte color){
     graphicsBuffer[graphicsBufferSize++] = 6;
     pushInt(x0);
     pushInt(y0);
@@ -95,19 +83,50 @@ void ConsoleGraphics::addTriangle(int x0, int y0, int x1, int y1, int x2, int y2
     pushInt(y1);
     pushInt(x2);
     pushInt(y2);
-    graphicsBuffer[graphicsBufferSize++] = color.b;
-    graphicsBuffer[graphicsBufferSize++] = color.g;
-    graphicsBuffer[graphicsBufferSize++] = color.r;
-    sendGraphics();
+    graphicsBuffer[graphicsBufferSize++] = color;
+    flushBuffer();
 }
 
-void ConsoleGraphics::addFillScreen(RGBColor color){
+void ConsoleGraphics::fillScreen(byte color){
     graphicsBuffer[graphicsBufferSize++] = 7;
-    graphicsBuffer[graphicsBufferSize++] = color.r;
-    graphicsBuffer[graphicsBufferSize++] = color.g;
-    graphicsBuffer[graphicsBufferSize++] = color.b;
-    sendGraphics();
+    graphicsBuffer[graphicsBufferSize++] = color;
+    flushBuffer();
 }
 
+void ConsoleGraphics::drawPixel(int x, int y, byte color){
+    graphicsBuffer[graphicsBufferSize++] = 8;
+    pushInt(x);
+    pushInt(y);
+    graphicsBuffer[graphicsBufferSize++] = color;
+    flushBuffer();
+}
 
+void ConsoleGraphics::storeSprite(byte bitmap[25], byte spriteIndex){
+    graphicsBuffer[graphicsBufferSize++] = 9;
+    graphicsBuffer[graphicsBufferSize++] = spriteIndex;
+    for(int i = 0; i < 25; i++){
+        graphicsBuffer[graphicsBufferSize++] = bitmap[i];
+    }
+    flushBuffer();
+}
 
+void ConsoleGraphics::drawSprite(byte spriteIndex, int x, int y, byte scaler){
+    graphicsBuffer[graphicsBufferSize++] = 10;
+    graphicsBuffer[graphicsBufferSize++] = spriteIndex;
+    pushInt(x);
+    pushInt(y);
+    graphicsBuffer[graphicsBufferSize++] = scaler;
+    flushBuffer();
+}
+
+void ConsoleGraphics::drawText(int x, int y, byte size, byte color, byte text[25]){
+    graphicsBuffer[graphicsBufferSize++] = 11;
+    pushInt(x);
+    pushInt(y);
+    graphicsBuffer[graphicsBufferSize++] = size;
+    graphicsBuffer[graphicsBufferSize++] = color;
+    for(int i = 0; i < 25; i++){
+        graphicsBuffer[graphicsBufferSize++] = text[i];
+    }
+    flushBuffer();
+}
--- ConsoleGraphics.h
+++ ConsoleGraphics.h
@@ -1,37 +1,54 @@
 #ifndef ConsoleGraphics_H
 #define ConsoleGraphics_H
 
+#include <Wire.h>
+
 #define byte uint8_t
 #define lowByte(w) ((uint8_t) ((w) & 0xff))
 #define highByte(w) ((uint8_t) ((w) >> 8))
 
-#include <Wire.h>
-
-typedef struct {
-  int r;
-  int g;
-  int b;
-}RGBColor;
+#define CLEAR       0
+#define BLACK       1
+#define NAVY        2
+#define DARKGREEN   3
+#define DARKCYAN    4
+#define MAROON      5
+#define PURPLE      6
+#define OLIVE       7
+#define LIGHTGREY   8
+#define DARKGREY    9
+#define BLUE        10
+#define GREEN       11
+#define CYAN        12
+#define RED         13
+#define MAGENTA     14
+#define YELLOW      15
+#define WHITE       16
+#define ORANGE      17
+#define GREENYELLOW 18
+#define PINK        19
 
 class ConsoleGraphics{
-  private:
     int slaveID;
-    
     
   public:
     int graphicsBufferSize = 0;  
     byte graphicsBuffer[32];
     ConsoleGraphics(int slaveID);
     void pushInt(int convInt);
-    void sendGraphics();
-    void addFastVLine(int x, int y, int length, RGBColor color);
-    void addFastHLine(int x, int y, int length, RGBColor color);
-    void addRect(int x, int y, int w, int h, RGBColor color);
-    void fillRect(int x, int y, int w, int h, RGBColor color);
-    void addCircle(int x, int y, int r, RGBColor color);
-    void fillCircle(int x, int y, int r, RGBColor color);
-    void addTriangle(int x0, int y0, int x1, int y1, int x2, int y2, RGBColor color);
-    void addFillScreen(RGBColor color);
+    void flushBuffer();
+    void drawFastVLine(int x, int y, int length, byte color);
+    void drawFastHLine(int x, int y, int length, byte color);
+    void addRect(int x, int y, int w, int h, byte color);
+    void fillRect(int x, int y, int w, int h, byte color);
+    void addCircle(int x, int y, int r, byte color);
+    void fillCircle(int x, int y, int r, byte color);
+    void addTriangle(int x0, int y0, int x1, int y1, int x2, int y2, byte color);
+    void fillScreen(byte color);
+    void drawPixel(int x, int y, byte color);
+    void storeSprite(byte bitmap[25], byte spriteIndex);
+    void drawSprite(byte spriteIndex, int x, int y, byte scaler);
+    void drawText(int x, int y, byte size, byte color, byte text[25]);
 };
 
 #endif
